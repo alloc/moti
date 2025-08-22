37ab026d00216c7c237c8e6bfd793facb51b457c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-08-22 14:58:10 -0400

    chore(types): add missing children type

--- packages/moti/src/interactions/pressable/hoverable.native.tsx
+++ packages/moti/src/interactions/pressable/hoverable.native.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSharedValue } from 'react-native-reanimated'
 import { HoveredContext } from './hoverable-context'
 
-export function Hoverable({ children }) {
+export function Hoverable({ children }: { children: React.ReactNode }) {
   return (
     <HoveredContext.Provider value={useSharedValue(false)}>
       {React.Children.only(children)}
